1d046fd05813c38229c4ea3cdc6ea57994368cdb
Siddhi Patel <114800007+iSiddhi@users.noreply.github.com>
2026-05-01 15:57:05 +0530

    Add Instructor and Learner models to admin

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -1,28 +1,31 @@
 from django.contrib import admin
-from .models import Course, Lesson, Question, Choice, Submission, Enrollment
+from .models import Course, Lesson, Question, Choice, Submission, Enrollment, Instructor, Learner
 
-# Inline for choices under question
+
 class ChoiceInline(admin.TabularInline):
     model = Choice
     extra = 2
 
-# Inline for questions under lesson
+
 class QuestionInline(admin.TabularInline):
     model = Question
     extra = 1
 
-# Admin for Question (shows choices inline)
+
 class QuestionAdmin(admin.ModelAdmin):
     inlines = [ChoiceInline]
 
-# Admin for Lesson (shows questions inline)
+
 class LessonAdmin(admin.ModelAdmin):
     inlines = [QuestionInline]
+    list_display = ['title']
 
-# Register models
+
 admin.site.register(Course)
 admin.site.register(Lesson, LessonAdmin)
 admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
 admin.site.register(Submission)
 admin.site.register(Enrollment)
+admin.site.register(Instructor)
+admin.site.register(Learner)
