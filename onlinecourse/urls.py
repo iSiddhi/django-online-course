285861f7818dd6ce837c34dba397f577e89fdf55
Siddhi Patel <114800007+iSiddhi@users.noreply.github.com>
2026-05-01 15:01:36 +0530

    Update exam view function in URL patterns

--- onlinecourse/urls.py
+++ onlinecourse/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('exam/', views.exam, name='exam'),
+    path('exam/', views.show_exam, name='exam'),
     path('submit/', views.submit, name='submit'),
     path('result/<int:submission_id>/', views.show_exam_result, name='result'),
 ]
