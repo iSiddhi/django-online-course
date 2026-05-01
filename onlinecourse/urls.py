5d451cb77139b2b67c56635d0a74d69b84262ca9
Siddhi Patel <114800007+iSiddhi@users.noreply.github.com>
2026-05-01 15:57:44 +0530

    Update URL patterns for exam views

--- onlinecourse/urls.py
+++ onlinecourse/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<int:course_id>/', views.show_exam, name='exam'),
+    path('exam/<int:course_id>/', views.show_exam, name='exam'),
     path('<int:course_id>/submit/', views.submit, name='submit'),
     path('course/<int:course_id>/submission/<int:submission_id>/result/',
          views.show_exam_result,
